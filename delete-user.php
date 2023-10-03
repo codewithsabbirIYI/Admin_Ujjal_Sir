fabdc6e6703ac0866bd6c54a7fd1cbb4c8a3022d
codewithsabbirIYI <tauhid10030@gmial.com>
2023-10-03 14:20:31 +0600

    user deleted successfully

---
+++ delete-user.php
@@ -0,0 +1,15 @@
+<?php
+    require_once('functions/functions.php');
+
+    $id = $_GET['d'];
+    
+
+    $delete_query = "DELETE FROM `users` WHERE `user_id` = '$id'";
+
+    if(mysqli_query($con, $delete_query)){
+        
+        header('Location: all-user.php');
+        echo "User Delete Successfully";
+    }
+
+?>
